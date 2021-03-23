d87083c1aa1f9626206e4c7331d99284c1f8e7d1
Natalia Gonzalez <nataliasita04@gmail.com>
2021-03-23 01:08:42 -0500

    maquetacion primeras 2 interfaces

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);//
+
+pages ();
+
+function pages () {
+
+    const botonInicio = document.getElementById("btnInicio");
+    botonInicio.onclick = function (){
+        document.querySelector(".interfazInicial").style.display="none";
+        document.querySelector(".header").style.display="block"; 
+    }
+
+}
