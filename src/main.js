601dff325d37b4175c4ebde5322487a31e010148
Natalia Gonzalez <nataliasita04@gmail.com>
2021-03-25 09:10:55 -0500

    terminancion de interfaz 1,2 incio de la interfaz 3

--- src/main.js
+++ src/main.js
@@ -3,22 +3,37 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-//console.log(example, data);//
+// eslint-disable-next-line no-console
+console.log(example,data);
 
 pages ();
 
 function pages () {
 
-    const botonInicio = document.getElementById("btnInicio");
+    let botonInicio = document.getElementById("btnInicio");
     botonInicio.onclick = function (){
         document.querySelector(".interfazInicial").style.display="none";
         document.querySelector(".header").style.display="block"; 
-        document.querySelector(".contentHome").style.display="block";
-        document.querySelector(".footer").style.display="block";  
-    
+        document.querySelector(".contentHome").style.display="block"; 
+        document.querySelector(".footer1").style.display="block"; 
+        document.querySelector(".contentPokedex").style.display="none";
     }
 
-}
+    let mainHome = document.getElementById("home");
+    mainHome.onclick = function(){
+        document.querySelector(".header").style.display="block"; 
+        document.querySelector(".contentHome").style.display="block"; 
+        document.querySelector(".footer1").style.display="block"; 
+        document.querySelector(".contentPokedex").style.display="none";
+    }
 
-
-
+    let mainPokedex = document.getElementById("pokedex");
+    mainPokedex.onclick = function(){
+        document.querySelector(".interfazInicial").style.display="none";
+        document.querySelector(".header").style.display="block"; 
+        document.querySelector(".contentHome").style.display="none"; 
+        document.querySelector(".footer1").style.display="none"; 
+        document.querySelector(".contentPokedex").style.display="block";
+        document.querySelector(".footer2").style.display="block"; 
+    }
+    }
