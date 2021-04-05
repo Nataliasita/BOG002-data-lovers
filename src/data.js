7bcf8aa19b51070f668b7864b52bfc5769bb8b36
Natalia Gonzalez <nataliasita04@gmail.com>
2021-04-05 03:08:13 -0500

    interfaz pokedex terminada data mostrada en tarjetas inicio de filtro y orden

--- src/data.js
+++ src/data.js
@@ -1,9 +1,56 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const general_fire =(pokemon)=>{
+  return pokemon.type[0] === "fire";
 };
-
-export const anotherExample = () => {
-  return 'OMG';
+export const general_grass =(pokemon)=>{
+  return pokemon.type[0] === "grass";
+};
+export const general_water =(pokemon)=>{
+  return pokemon.type[0] === "water";
+};
+export const general_bug =(pokemon)=>{
+  return pokemon.type[0] === "bug";
+};
+export const general_normal =(pokemon)=>{
+  return pokemon.type[0] === "normal";
+};
+export const general_poison =(pokemon)=>{
+  return pokemon.type[0] === "poison";
+};
+export const general_flying =(pokemon)=>{
+  return pokemon.type[1] === "flying";
+};
+export const general_electric =(pokemon)=>{
+  return pokemon.type[0] === "electric";
+};
+export const general_ground =(pokemon)=>{
+  return pokemon.type[0] === "ground";
+};
+export const general_fighting =(pokemon)=>{
+  return pokemon.type[0] === "fighting";
+};
+export const general_psychic =(pokemon)=>{
+  return pokemon.type[0] === "psychic";
+};
+export const general_rock =(pokemon)=>{
+  return pokemon.type[0] === "rock";
+};
+export const general_ghost =(pokemon)=>{
+  return pokemon.type[0] === "ghost";
+};
+export const general_dragon =(pokemon)=>{
+  return pokemon.type[0] === "dragon";
+};
+export const general_fairy =(pokemon)=>{
+  return pokemon.type[0] === "fairy";
+};
+export const general_dark =(pokemon)=>{
+  return pokemon.type[0] === "dark";
+};
+export const general_steel =(pokemon)=>{
+  return pokemon.type[0] === "steel";
+};
+export const general_ice =(pokemon)=>{
+  return pokemon.type[0] === "ice";
 };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import { example } from './data.js';
+import { general_fire } from './data.js';
+import { general_grass } from './data.js';
+import { general_water } from './data.js';
+import { general_bug } from './data.js';
+import { general_normal } from './data.js';
+import { general_poison } from './data.js';
+import { general_flying } from './data.js';
+import { general_electric } from './data.js';
+import { general_ground } from './data.js';
+import { general_fighting } from './data.js';
+import { general_psychic } from './data.js';
+import { general_rock } from './data.js';
+import { general_ghost } from './data.js';
+import { general_dragon } from './data.js';
+import { general_fairy } from './data.js';
+import { general_dark } from './data.js';
+import { general_steel } from './data.js';
+import { general_ice } from './data.js';
 
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 
 const infoGeneral = data.pokemon;
-
-
 // eslint-disable-next-line no-console
 //console.log(example,data);
 
@@ -52,48 +68,84 @@ infoGeneral.forEach (poke => {
     num_pokemon.textContent = poke.num;
     num_pokemon.className = "numPoke";
     card_pokemon.appendChild(num_pokemon);
+    const information_pokemon =document.createElement("figcaption");
+    information_pokemon.className ="information";
+    card_pokemon.appendChild(information_pokemon);
     const type1_pokemon =document.createElement("p");
-    type1_pokemon.textContent = poke.type[0];
+    type1_pokemon.textContent = "Tipo : "+ poke.type[0];
     type1_pokemon.className = "typesPoke";
-    card_pokemon.appendChild(type1_pokemon);
+    information_pokemon.appendChild(type1_pokemon);
     const type2_pokemon =document.createElement("p");
-    type2_pokemon.textContent = poke.type[1];
-    type2_pokemon.className = "typesPoke";
-    card_pokemon.appendChild(type2_pokemon);
+    type2_pokemon.textContent =poke.type[1];
+    type2_pokemon.className = "typesPoke2";
+    information_pokemon.appendChild(type2_pokemon);
+    const atributes_pokemon_height =document.createElement("p");
+    atributes_pokemon_height.textContent = "Altura : "+ poke.size.height;
+    atributes_pokemon_height.className = "atributes";
+    information_pokemon.appendChild(atributes_pokemon_height);
+    const atributes_pokemon_weight =document.createElement("p");
+    atributes_pokemon_weight.textContent ="Peso : "+ poke.size.weight;
+    atributes_pokemon_weight.className = "atributes";
+    information_pokemon.appendChild(atributes_pokemon_weight);
+    
 })
 
-//const pokeName =[]
-//data.pokemon.forEach(poke =>{
-//pokeName.push(poke.name)
 
-//})
-//console.log(pokeName)
+// funciones de filtro
 
-//const pokeNum = []
-//data.pokemon.forEach(poke =>{
-//    pokeNum.push(poke.num)    
-//})
-//console.log(pokeNum)
+const pokemon_fire =infoGeneral.filter(general_fire).toLowerCase;
+//console.log(pokemon_fire)
 
-//const pokeImg = []
-//data.pokemon.forEach(poke =>{
-//    pokeImg.push(poke.img)
-//})
-//console.log(pokeImg)
+const pokemon_grass =infoGeneral.filter(general_grass);
+//console.log(pokemon_grass)
 
-//const poketype1 = []
-//data.pokemon.forEach(poke =>{
-//    poketype1.push(poke.type[0])
-//})
-//console.log(poketype1)
+const pokemon_water =infoGeneral.filter(general_water);
+//console.log(pokemon_water)
 
-//const poketype2 = []
-//data.pokemon.forEach(poke =>{
-//    poketype2.push(poke.type[1])
-//})
-//console.log(poketype2)
+const pokemon_bug =infoGeneral.filter(general_bug);
+//console.log(pokemon_bug)
 
+const pokemon_normal =infoGeneral.filter(general_normal);
+//console.log(pokemon_normal)
 
+const pokemon_poison =infoGeneral.filter(general_poison);
+//console.log(pokemon_poison)
+
+const pokemon_flying =infoGeneral.filter(general_flying);
+//console.log(pokemon_flying)
+
+const pokemon_electric =infoGeneral.filter(general_electric);
+//console.log(pokemon_electric)
+
+const pokemon_ground =infoGeneral.filter(general_ground);
+//console.log(pokemon_ground)
+
+const pokemon_fighting =infoGeneral.filter(general_fighting);
+//console.log(pokemon_fighting)
+
+const pokemon_psychic =infoGeneral.filter(general_psychic);
+//console.log(pokemon_psychic)
+
+const pokemon_rock =infoGeneral.filter(general_rock);
+//console.log(pokemon_rock)
+
+const pokemon_ghost =infoGeneral.filter(general_ghost);
+//console.log(pokemon_ghost)
+
+const pokemon_dragon =infoGeneral.filter(general_dragon);
+//console.log(pokemon_dragon)
+
+const pokemon_fairy =infoGeneral.filter(general_fairy);
+//console.log(pokemon_fairy)
+
+const pokemon_dark =infoGeneral.filter(general_dark);
+//console.log(pokemon_dark)
+
+const pokemon_steel =infoGeneral.filter(general_steel);
+//console.log(pokemon_steel)
+
+const pokemon_ice =infoGeneral.filter(general_ice);
+//console.log(pokemon_ice)
 
 pages ();
 
@@ -143,5 +195,4 @@ function pages () {
 //    let pokeImg = infoGeneral[i].img;
 //    let poketype1 = infoGeneral[i].type[0];
 //    let poketype2 = infoGeneral[i].type[1];
-
 //}
